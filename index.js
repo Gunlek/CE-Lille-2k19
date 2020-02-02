c429b57cdbb2747908141a3c1096575f8b601fa4
Fabien Aubret <fabien.aubret@gmail.com>
2020-02-02 16:59:28 +0100

    [Update] Update design

--- index.js
+++ index.js
@@ -10,7 +10,27 @@ app.use(session({
 app.use(express.static('statics'));
 
 app.get('/', (req, res) => {
-    res.render('home/home.html.twig');
+    res.render('home.html.twig');
+});
+
+app.get('/le-ce/', (req, res) => {
+    res.render('ce.html.twig');
+});
+
+app.get('/ateliers/', (req, res) => {
+    res.render('ateliers.html.twig');
+});
+
+app.get('/billetterie/', (req, res) => {
+    res.render('billetterie.html.twig');
+});
+
+app.get('/organisation/', (req, res) => {
+    res.render('organisation.html.twig');
+});
+
+app.get('/contact/', (req, res) => {
+    res.render('contact.html.twig');
 });
 
 app.listen(8080);
